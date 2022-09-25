8c8904cf8d0b93055105c4ef3f16ac23e2e1c4d4
Samuel Seidel <samuelsc2518@gmail.com>
2022-09-25 17:00:36 -0500

    Created window with background color.

--- Main.cpp
+++ Main.cpp
@@ -4,35 +4,53 @@
 
 int main() {
 
+	// Initialize GLFW
 	glfwInit();
 
+	// Tell GLFW what version of OpenGL we are using
+	// In this case 3.1 because is my version in my pc
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	// Can't user OpenGL Profile
 	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); my opengl version is 3.1 and this is for 3.2 and above
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// create GLFW Window with 800 x 800 px, and naming it "Test"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
 
+	// error check if the window have problem when creating
 	if (window == NULL) {
 		const char* description;
 		int code = glfwGetError(&description);
-		std::cout << "Failed to create the GLFW Window. Error code: " << code << ". Description: " << description << std::endl;
+		std::cout << "Failed to create the GLFW Window. Error: " << description << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
+	// Load GLAD so it configures OpenGL
 	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
 
+	// Specify the color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
 	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
 	glfwSwapBuffers(window);
 
+	// Main while loop
 	while (!glfwWindowShouldClose(window)) {
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 
 	return 0;
