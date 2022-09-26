1e35128a3c8fe789cc3ea5d9df517aa6dba503e2
Samuel Seidel <samuelsc2518@gmail.com>
2022-09-25 23:12:02 -0500

    Create 3 triangles using Element Buffer and indices

--- Main.cpp
+++ Main.cpp
@@ -20,11 +20,12 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 
 // some constants coordinates predefined for next use
-const GLfloat topY = 0.5f * float(sqrt(3)) * 2 / 3;
-const GLfloat bottomY = -0.5f * float(sqrt(3)) / 3;
-const GLfloat leftX = -0.5f;
-const GLfloat rightX = 0.5f;
-const GLfloat middle = 0.0f;
+const GLfloat topY = 0.5f * float(sqrt(3)) * 2 / 3; // the top in the Y axis
+const GLfloat bottomY = -0.5f * float(sqrt(3)) / 3; // the bottom in the Y axis
+const GLfloat leftX = -0.5f; // The left in the X axis
+const GLfloat rightX = 0.5f; // the right in the X axis
+const GLfloat middle = 0.0f; // the middle in both Y and X axis. The point (0,0)
+const GLfloat innerMiddleY = 0.5f * float(sqrt(3)) / 6; // inner middle in Y axis of the line between lowers vectors and upper vector
 
 int main() {
 
@@ -43,11 +44,21 @@ int main() {
 	{
 		leftX, bottomY, 0.0f, // Lower left corner
 		rightX, bottomY, 0.0f, // Lower right corner
-		middle, topY, 0.0f // Upper middle corner
+		middle, topY, 0.0f, // Upper middle corner
+		leftX / 2, innerMiddleY, 0.0f, // Inner left
+		rightX / 2, innerMiddleY, 0.0f, // Inner right
+		middle, bottomY, 0.0f // Inner down
+	};
+
+	GLuint indices[] =
+	{
+		0, 3, 5, // lower left triangle
+		1, 4, 5, // lower right triangle
+		2, 3, 4 // upper triangle
 	};
 
 	// create GLFW Window with 800 x 800 px, and naming it "Test"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Test", NULL, NULL);
 
 	// error check if the window have problem when creating
 	if (window == NULL) {
@@ -65,7 +76,7 @@ int main() {
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, 600, 600);
 
 
 	// Create vertex shader object and get its reference
@@ -96,12 +107,13 @@ int main() {
 
 
 
-	// Create the reference containers for the Vertex Array Object and the Vertex Buffer Object
-	GLuint VAO, VBO;
+	// Create the reference containers for the Vertex Array Object, the Vertex Buffer Object and the Element Buffer Object
+	GLuint VAO, VBO, EBO;
 
-	// Generate the VAO and VBO with only 1 object each
+	// Generate the VAO, VBO and EBO with only 1 object each
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// Make the VAO the current Vertex Array Object by binding it
 	glBindVertexArray(VAO);
@@ -111,14 +123,20 @@ int main() {
 	// Introduce the vertices into the VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Binding the EBO specifying it's a GL_ELEMENT_ARRAY_BUFFER
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// Introduce the indices into the EBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	// Enable the Vertex Attribute so that OpenGL knows to use it
 	glEnableVertexAttribArray(0);
 
-	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+	// Bind all three, the VBO, VAO and EBO to 0 so that we don't accidentally modify the VAO and VBO we created
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Specify the color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -135,8 +153,8 @@ int main() {
 		glUseProgram(shaderProgram);
 		// Bind the VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Draw the triangles using the GL_TRIANGLES primitive and the number of vertices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
@@ -145,6 +163,7 @@ int main() {
 	// Delete all the objects that we created
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// Delete window before ending the program
